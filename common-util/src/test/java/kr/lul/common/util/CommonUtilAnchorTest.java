40dd2b8eff55c4b867508efb8c3813f4f4c53b89
Just Burrow <just.burrow@lul.kr>
2019-11-04 13:15:50 +0900

    ADD : `Anchor`

---
+++ common-util/src/test/java/kr/lul/common/util/CommonUtilAnchorTest.java
@@ -0,0 +1,22 @@
+package kr.lul.common.util;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.slf4j.Logger;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * @author justburrow
+ * @since 2019/11/04
+ */
+public class CommonUtilAnchorTest {
+  private static final Logger log = getLogger(CommonUtilAnchorTest.class);
+
+  @Test
+  public void test_constructor() throws Exception {
+    Assertions.assertThatThrownBy(CommonUtilAnchor::new)
+        .isInstanceOf(UnsupportedOperationException.class)
+        .hasMessage("does not support instance.");
+  }
+}
