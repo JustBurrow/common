9c64333e609812c84d803dcd04fea29e07a06077
Just Burrow <just.burrow@lul.kr>
2020-01-05 15:02:14 +0900

    UPDATE : 정규표현식 검증기를 개선.

--- common-util/src/main/java/kr/lul/common/util/validator/RegexValidator.java
+++ common-util/src/main/java/kr/lul/common/util/validator/RegexValidator.java
@@ -3,42 +3,92 @@ package kr.lul.common.util.validator;
 import kr.lul.common.util.AbstractTargetNameDefinedValidator;
 import kr.lul.common.util.ValidationException;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static java.lang.String.format;
-import static kr.lul.common.util.Arguments.matches;
+import static kr.lul.common.util.Arguments.notEmpty;
 import static kr.lul.common.util.Arguments.notNull;
+import static kr.lul.common.util.Texts.singleQuote;
 
 /**
+ * 정규표현식으로 문자열을 검증하는 검증기.
+ *
  * @author justburrow
  * @since 2019/11/16
  */
-public class RegexValidator<T> extends AbstractTargetNameDefinedValidator<String> {
+public class RegexValidator extends AbstractTargetNameDefinedValidator<String> {
   private String regex;
+  private Pattern pattern;
+  private int flags;
 
-  public RegexValidator(String target, String regex) {
+  /**
+   * 기본 옵션을 사용.
+   *
+   * @param target 대상 오브젝트 이름.
+   * @param regex  정규표현식.
+   */
+  public RegexValidator(final String target, final String regex) {
+    this(target, regex, 0);
+  }
+
+  /**
+   * 정규표현식 옵션을 사용.
+   *
+   * @param target 대상 오브젝트 이름.
+   * @param regex  정규표현식.
+   * @param flags  정규표현식 옵션.
+   *
+   * @see Pattern#compile(String, int) 참고.
+   */
+  public RegexValidator(final String target, final String regex, final int flags) {
     super(target);
+    notEmpty(regex, "regex");
 
-    Pattern.compile(regex);
     this.regex = regex;
+    this.pattern = Pattern.compile(regex, flags);
+    this.flags = flags;
   }
 
   public String getRegex() {
     return this.regex;
   }
 
+  public Pattern getPattern() {
+    return this.pattern;
+  }
+
+  public int getFlags() {
+    return this.flags;
+  }
+
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // kr.lul.common.util.Validator
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   @Override
-  public void validate(String target) throws ValidationException {
+  public void validate(final String target) throws ValidationException {
     try {
       notNull(target, this.targetName);
-      matches(target, this.regex, this.targetName);
-    } catch (IllegalArgumentException e) {
+
+      final Matcher matcher = this.pattern.matcher(target);
+      if (!matcher.matches())
+        throw new ValidationException(this.targetName, target, format("illegal %s pattern : %s=%s, pattern=%s",
+            this.targetName, this.targetName, singleQuote(target), this.pattern));
+    } catch (final IllegalArgumentException e) {
       throw new ValidationException(this.targetName, target, e);
     }
   }
 
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // java.lang.Object
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   @Override
   public String toString() {
-    return format("%s{targetName='%s', regex='%s'}}", RegexValidator.class.getSimpleName(), this.targetName, this.regex);
+    return new StringBuilder(RegexValidator.class.getSimpleName())
+               .append("{targetName=").append(singleQuote(this.targetName))
+               .append("regex=").append(singleQuote(this.regex))
+               .append(", pattern=").append(this.pattern)
+               .append(", flags=").append(this.flags)
+               .append('}').toString();
   }
 }
---
+++ common-util/src/test/java/kr/lul/common/util/validator/RegexValidatorTest.java
@@ -0,0 +1,72 @@
+package kr.lul.common.util.validator;
+
+import kr.lul.common.util.ValidationException;
+import org.junit.Test;
+import org.slf4j.Logger;
+
+import java.util.regex.Pattern;
+
+import static java.util.regex.Pattern.MULTILINE;
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowableOfType;
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * @author justburrow
+ * @since 2020/01/05
+ */
+public class RegexValidatorTest {
+  private static final Logger log = getLogger(RegexValidatorTest.class);
+
+  @Test
+  public void test_validate_case_insensitive() throws Exception {
+    // GIVEN
+    final RegexValidator validator = new RegexValidator("target", "[a-z]+", Pattern.CASE_INSENSITIVE);
+    log.info("GIVEN - validator={}", validator);
+
+    // WHEN
+    validator.validate("a");
+    validator.validate("A");
+
+    // THEN
+    log.info("THEN - OK");
+  }
+
+  @Test
+  public void test_validate_case_sensitive() throws Exception {
+    // GIVEN
+    final RegexValidator validator = new RegexValidator("target", "[a-z]+");
+    log.info("GIVEN - validator={}", validator);
+
+    // WHEN
+    validator.validate("a");
+    final ValidationException ex = catchThrowableOfType(() -> validator.validate("A"), ValidationException.class);
+    log.info("WHEN - ex=" + ex, ex);
+
+    // THEN
+    assertThat(ex)
+        .isNotNull()
+        .hasMessageStartingWith("illegal target pattern : target=A");
+  }
+
+  @Test
+  public void test_validate_multi_line() throws Exception {
+    // GIVEN
+    final RegexValidator validator = new RegexValidator("target", "\\S+", MULTILINE);
+    log.info("GIVEN - validator={}", validator);
+
+    // WHEN
+    validator.validate(randomAlphabetic(1, 10));
+    final ValidationException ex = catchThrowableOfType(
+        () -> validator.validate(randomAlphanumeric(1, 10) + "\n" + randomAlphanumeric(1, 10)),
+        ValidationException.class);
+    log.info("WHEN - ex=" + ex, ex);
+
+    // THEN
+    assertThat(ex)
+        .isNotNull()
+        .hasMessageStartingWith("illegal target pattern");
+  }
+}
