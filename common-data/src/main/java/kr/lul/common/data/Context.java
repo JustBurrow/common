35553ce86583e99987fa36b90beb84aba6c0ca45
Just Burrow <just.burrow@lul.kr>
2020-04-26 15:11:36 +0900

    DELETE : `ContextContainer`를 삭제. `Context`에 통합.

--- common-data/src/main/java/kr/lul/common/data/Context.java
+++ common-data/src/main/java/kr/lul/common/data/Context.java
@@ -1,30 +1,41 @@
 package kr.lul.common.data;
 
+import java.util.Objects;
 import java.util.UUID;
 
+import static kr.lul.common.util.Arguments.notNull;
+
 /**
+ * 컨텍스트.
+ *
  * @author justburrow
- * @since 2019/12/07
+ * @since 2019/12/12
  */
 public class Context {
+  /**
+   * 컨텍스트 ID.
+   */
   protected final UUID id;
 
-  public Context() {
-    this(UUID.randomUUID());
-  }
+  public Context(final UUID id) {
+    notNull(id, "id");
 
-  public Context(UUID id) {
-    if (null == id)
-      throw new IllegalArgumentException("contextId is null.");
     this.id = id;
   }
 
-  public UUID id() {
+  public Context(final Context context) {
+    this(context.id);
+  }
+
+  public UUID getId() {
     return this.id;
   }
 
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // java.lang.Object
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   @Override
-  public boolean equals(Object o) {
+  public boolean equals(final Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     return this.id.equals(((Context) o).id);
@@ -32,7 +43,7 @@ public class Context {
 
   @Override
   public int hashCode() {
-    return this.id.hashCode();
+    return Objects.hash(this.id);
   }
 
   @Override
