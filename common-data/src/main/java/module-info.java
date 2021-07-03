16a7256596cf04065a2c19904a7cddea9d21c79e
Just Burrow <just.burrow@lul.kr>
2021-07-03 12:52:52 +0900

    ➕ Jigsaw 모듈 정의.

---
+++ common-data/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * @author justburrow
+ * @since 2021/07/02
+ */
+module kr.lul.common.data {
+  requires kr.lul.common.util;
+
+  exports kr.lul.common.data;
+}
---
+++ common-util/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * @author justburrow
+ * @since 2021/07/02
+ */
+module kr.lul.common.util {
+  exports kr.lul.common.util;
+  exports kr.lul.common.util.composer;
+  exports kr.lul.common.util.parser;
+  exports kr.lul.common.util.validator;
+}
